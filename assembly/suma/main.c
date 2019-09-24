6f28079af0b0ba98bbfaebecf68d6c969ee5197a
Josefina Montenegro <josefina.montenegro@despegar.com>
2019-09-24 18:16:50 -0300

    Ejemplo de suma, hay que probarlo en netbsd

---
+++ assembly/suma/main.c
@@ -0,0 +1,12 @@
+#include "suma.h"
+
+int main() {
+    int n1, n2;
+
+	scanf("%d", &n1);
+	scanf("%d", &n2);
+
+	int res = suma(n1, n2)
+
+    return res;
+}
---
+++ assembly/suma/suma.h
@@ -0,0 +1,6 @@
+#ifndef _suma_H
+#define _suma_H
+
+int suma(unsigned int n1, unsigned int n2);
+
+#endif
